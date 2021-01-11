82f62272e04d57590322d177e8660b68c7c87139
Sondre Nordjore <sonoskole@outlook.com>
2021-01-11 13:32:50 +0100

    update header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Logo from "../../images/layout/logo_text_100.png";
 
 function Header() {
   return (
     <div className="header">
+      <img src={Logo} alt="Holidaze logo" />
       <h1>Header goes here</h1>
     </div>
   );
