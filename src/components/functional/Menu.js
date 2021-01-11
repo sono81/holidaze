7789ec74f4681ae839cbc47f3918b34fbce1600d
Sondre Nordjore <sonoskole@outlook.com>
2021-01-11 15:58:55 +0100

    add menu component

---
+++ src/components/functional/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Menu () {
+    return (
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+        </ul>
+    )
+}
+
+export default Menu;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Logo from "../../images/layout/logo_text_100.png";
+import Menu from "../functional/Menu";
 
 function Header() {
   return (
     <div className="header">
       <img src={Logo} alt="Holidaze logo" />
-      <h1>Header goes here</h1>
+      <Menu />
     </div>
   );
 }
