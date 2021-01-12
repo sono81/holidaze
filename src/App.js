3a7fd7eaf1aa12f6f5b6e0fcda683de37dda4e92
Sondre Nordjore <sonoskole@outlook.com>
2021-01-12 10:44:57 +0100

    change Header to Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './main.scss';
-import Header from './components/layout/Header';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Layout />
     </div>
   );
 }
