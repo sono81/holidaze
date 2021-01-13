17ca84cc9ef749b38cc7b494bf9c717b6f25c007
Sondre Nordjore <sonoskole@outlook.com>
2021-01-13 11:15:23 +0100

    get results from api to LandingPage

---
+++ src/components/layout/EstablishmentItems.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EstablishmentItems({establishmentName}) {
+    return <h2> {establishmentName} </h2>
+}
+
+export default EstablishmentItems;
--- src/components/layout/LandingPage.js
+++ src/components/layout/LandingPage.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Banner from "./Banner";
+import { GET_ESTABLISHMENTS } from "../../constants/api";
+import EstablishmentItems from "./EstablishmentItems";
 
 function LandingPage() {
+  const [establishments, setEstablishments] = useState([]);
+
+  useEffect(() => {
+    fetch(GET_ESTABLISHMENTS)
+      .then((response) => response.json())
+      .then((json) => {
+        setEstablishments(json);
+        console.log(json);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <Banner />
       <h1>LandingPage</h1>
+      {establishments.map((data) => {
+        const { establishmentName } = data;
+
+        return (
+          <EstablishmentItems 
+          establishmentName={establishmentName} 
+          />
+        );
+      })}
     </>
   );
 }
