f13870de12b48a378a8569b3edebd1e26645cdaa
Sondre Nordjore <sonoskole@outlook.com>
2021-01-28 18:07:08 +0100

    Add content from api to Messages.js

--- src/components/functional/Dashboard/Messages.js
+++ src/components/functional/Dashboard/Messages.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { GET_MESSAGES } from "../../../constants/api";
 
 function Messages() {
+  const [data, setMessages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(GET_MESSAGES)
+      .then((response) => response.json())
+      .then((json) => {
+        setMessages(json);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  console.log(data);
+
   return (
-      <div className="message__wrap">
-        <h2 className="message__wrap--heading">Messages</h2>
-        <div className="message__container">
-          <div className="message__item">
-            <h3 className="message__item--heading">Message 1</h3>
-            <p>Message content goes here</p>
-          </div>
-          <hr />
-          <div className="message__item">
-            <h3 className="message__item--heading">Message 1</h3>
-            <p>Message content goes here</p>
-          </div>
-          <hr />
+    <div className="message__wrap">
+      <h2 className="message__wrap--heading">Messages</h2>
+      <div className="message__container">
+        <div className="message__item">{
+            data.map((messages) => {
+              const {clientName, email, message} = messages;
+              return (
+                <>
+                <h3 className="message__item--heading">{clientName}</h3>
+                <p>Email: {email}</p>
+                <p>Message: {message}</p>
+                <hr />
+                </>
+              );
+            })}
         </div>
       </div>
+    </div>
   );
 }
 
--- src/constants/api.js
+++ src/constants/api.js
@@ -1,1 +1,2 @@
 export const GET_ESTABLISHMENTS = "http://localhost:80/hotel-api/get-establishments.php";
+export const GET_MESSAGES = "http://localhost:80/hotel-api/get-contacts.php";
