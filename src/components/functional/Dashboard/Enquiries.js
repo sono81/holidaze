b069d818fe7335198274251e1667997cbd672f4a
Sondre Nordjore <sonoskole@outlook.com>
2021-01-28 15:10:08 +0100

    Styled messages and enquiries components

--- src/components/functional/Dashboard/Enquiries.js
+++ src/components/functional/Dashboard/Enquiries.js
@@ -3,15 +3,15 @@ import React from "react";
 function Enquiries() {
   return (
     <div className="message__wrap">
-      <h2>Enquiries</h2>
-      <div className="message__containter">
+      <h2 className="message__wrap--heading">Enquiries</h2>
+      <div className="message__container">
           <div className="message__item">
-              <h3>Enquirie 1</h3>
+              <h3 className="message__item--heading">Enquirie 1</h3>
               <p>Enquirie content goes here</p>
           </div>
           <hr />
           <div className="message__item">
-              <h3>Enqueirie 2</h3>
+              <h3 className="message__item--heading">Enqueirie 2</h3>
               <p>Enquirie content goes here</p>
           </div>
           <hr />
