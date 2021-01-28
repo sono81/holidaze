8aeff029171646a999db2ae6767abcda0857cecc
Sondre Nordjore <sonoskole@outlook.com>
2021-01-28 18:26:12 +0100

    Add content from api to Enquiries.js

--- src/components/functional/Dashboard/Enquiries.js
+++ src/components/functional/Dashboard/Enquiries.js
@@ -1,20 +1,57 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
+import { GET_ENQUIRIES } from "../../../constants/api";
 
 function Enquiries() {
+  const [data, setEnquiries] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(GET_ENQUIRIES)
+      .then((response) => response.json())
+      .then((json) => {
+        setEnquiries(json);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return null;
+  }
+
   return (
     <div className="message__wrap">
       <h2 className="message__wrap--heading">Enquiries</h2>
       <div className="message__container">
-          <div className="message__item">
-              <h3 className="message__item--heading">Enquirie 1</h3>
-              <p>Enquirie content goes here</p>
-          </div>
-          <hr />
-          <div className="message__item">
-              <h3 className="message__item--heading">Enqueirie 2</h3>
-              <p>Enquirie content goes here</p>
-          </div>
-          <hr />
+        <div className="message__item">
+          {data.map((enquiries) => {
+            const {
+              checkin,
+              checkout,
+              clientName,
+              email,
+              establishment,
+            } = enquiries;
+            return (
+              <Fragment key={email}>
+                <h3 className="message__item--heading">{establishment}</h3>
+                <p>
+                  <b>Name:</b> {clientName}
+                </p>
+                <p>
+                  <b>Email:</b> {email}
+                </p>
+                <p>
+                  <b>Check in:</b> {checkin}
+                </p>
+                <p>
+                  <b>Check out:</b> {checkout}
+                </p>
+                <hr />
+              </Fragment>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,3 @@
 export const GET_ESTABLISHMENTS = "http://localhost:80/hotel-api/get-establishments.php";
 export const GET_MESSAGES = "http://localhost:80/hotel-api/get-contacts.php";
+export const GET_ENQUIRIES = "http://localhost:80/hotel-api/get-enquiries.php";
