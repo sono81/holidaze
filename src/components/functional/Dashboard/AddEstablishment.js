6aa39e7aec1d04f6e437170b19a7b26c55d56d22
Sondre Nordjore <sonoskole@outlook.com>
2021-01-28 16:21:35 +0100

    Made two columns in addestablishment

--- src/components/functional/Dashboard/AddEstablishment.js
+++ src/components/functional/Dashboard/AddEstablishment.js
@@ -5,32 +5,38 @@ function AddEstablishment() {
     <div className="message__wrap message__wrap--wide">
       <h2 className="message__wrap--heading">Add Establishment</h2>
       <form className="message__item">
-        <label className="message__item--label">Name:</label>
-        <input type="text" className="message__item--input"/>
-        <label className="message__item--label">Email:</label>
-        <input type="email" className="message__item--input"/>
-        <label className="message__item--label">Google Lat:</label>
-        <input type="number" className="message__item--input"/>
-        <label className="message__item--label">Google Lng:</label>
-        <input type="number" className="message__item--input"/>
-        <p className="message__item--label">
-          Self Catering:
-          <label>
-            <input type="radio" value="yes" name="selfCatering" />
-            Yes
-          </label>
-          <label>
-            <input type="radio" value="no" name="selfCatering" />
-            No
-          </label>
-        </p>
-        <label className="message__item--label">Price:</label>
-        <input type="number" className="message__item--input"/>
-        <label className="message__item--label">Max Guests:</label>
-        <input type="number" className="message__item--input"/>
+        <div className="message__item--left">
+          <label className="message__item--label">Name:</label>
+          <input type="text" className="message__item--input" />
+          <label className="message__item--label">Email:</label>
+          <input type="email" className="message__item--input" />
+          <label className="message__item--label">Google Lat:</label>
+          <input type="number" className="message__item--input" />
+          <label className="message__item--label">Google Lng:</label>
+          <input type="number" className="message__item--input" />
+        </div>
+        <div className="message__item--right">
+          <p className="message__item--label">
+            Self Catering:
+            <label>
+              <input type="radio" value="yes" name="selfCatering" />
+              Yes
+            </label>
+            <label>
+              <input type="radio" value="no" name="selfCatering" />
+              No
+            </label>
+          </p>
+          <label className="message__item--label">Price:</label>
+          <input type="number" className="message__item--input" />
+          <label className="message__item--label">Max Guests:</label>
+          <input type="number" className="message__item--input" />
+        </div>
         <label className="message__item--label">Description:</label>
         <textarea className="message__item--textarea"></textarea>
-        <button className="btn__add btn--greenthin" type="submit">Add</button>
+        <button className="btn__add btn--greenthin" type="submit">
+          Add
+        </button>
       </form>
     </div>
   );
