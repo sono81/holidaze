b1e36765fabb3863dd754d621653e6be89f5569f
Sondre Nordjore <sonoskole@outlook.com>
2021-01-29 14:47:55 +0100

    Add constants for POST

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,6 @@
 export const GET_ESTABLISHMENTS = "http://localhost:80/hotel-api/get-establishments.php";
 export const GET_MESSAGES = "http://localhost:80/hotel-api/get-contacts.php";
 export const GET_ENQUIRIES = "http://localhost:80/hotel-api/get-enquiries.php";
+export const POST_ESTABLISHMENTS = "http://localhost:80/hotel-api/add-establishment-success.php";
+export const POST_MESSAGES = "http://localhost:80/hotel-api/contact-success.php";
+export const POST_ENQUIRIES = "http://localhost:80/hotel-api/enquiry-success.php";
