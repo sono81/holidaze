3c702375764b80f4d434ba6636d1e5fb1ec0e871
Sondre Nordjore <sonoskole@outlook.com>
2021-02-02 20:56:51 +0100

    Update on searchbar

--- src/components/functional/SearchBar.js
+++ src/components/functional/SearchBar.js
@@ -1,27 +1,125 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-function SearchBar({ handleSearch, value, id }) {
-  const updateSearch = (input) => {
-    handleSearch(input);
+export class SearchBar extends Component {
+  static propTypes = {
+    options: PropTypes.instanceOf(Array).isRequired,
+  };
+  state = {
+    activeOption: 0,
+    filteredOptions: {},
+    showOptions: false,
+    userInput: "",
   };
 
-  console.log(value);
+  onChange = (e) => {
+    console.log("onChanges");
 
-  return (
-    <>
-      <input
-        type="text"
-        className="searchbar"
-        placeholder="Find a place to stay..."
-        onChange={(e) => updateSearch(e)}
-      />
+    const { options } = this.props;
+    const userInput = e.currentTarget.value;
 
-      <Link to={"establishment/" + id}>
-        <div className="searchbar__results">{value}</div>
-      </Link>
-    </>
-  );
+    const filteredOptions = options.filter(
+      (optionName) =>
+        optionName.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+    );
+
+    this.setState({
+      activeOption: 0,
+      filteredOptions,
+      showOptions: true,
+      userInput: e.currentTarget.value,
+    });
+  };
+
+  onClick = (e) => {
+    this.setState({
+      activeOption: 0,
+      filteredOptions: [],
+      showOptions: false,
+      userInput: e.currentTarget.innerText,
+    });
+  };
+  onKeyDown = (e) => {
+    const { activeOption, filteredOptions } = this.state;
+
+    if (e.keyCode === 13) {
+      this.setState({
+        activeOption: 0,
+        showOptions: false,
+        userInput: filteredOptions[activeOption],
+      });
+    } else if (e.keyCode === 38) {
+      if (activeOption === 0) {
+        return;
+      }
+      this.setState({ activeOption: activeOption - 1 });
+    } else if (e.keyCode === 40) {
+      if (activeOption === filteredOptions.length - 1) {
+        console.log(activeOption);
+        return;
+      }
+      this.setState({ activeOption: activeOption + 1 });
+    }
+  };
+
+  render() {
+
+    const {
+      onChange,
+      onClick,
+      onKeyDown,
+
+      state: { activeOption, filteredOptions, showOptions, userInput },
+    } = this;
+    let optionList;
+
+
+    
+    if (showOptions && userInput) {
+      if (filteredOptions.length) {
+        optionList = (
+          <ul className="searchbar__results">
+            {filteredOptions.map((optionName, index) => {
+              let className;
+
+              if (index === activeOption) {
+                className = "searchbar__results--active";
+              }
+              return (
+                <li className={className} key={optionName} onClick={onClick}>
+                  <Link to={"establishment/" } style={{textDecoration: "none", color: "black"}} className="">
+                  {optionName}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        );
+      } else {
+        optionList = (
+          <div className="searchbar__results--none">
+            <em>No Option!</em>
+          </div>
+        );
+      }
+    }
+    return (
+      <React.Fragment>
+        <div className="search">
+          <input
+            type="text"
+            className="searchbar"
+            placeholder="Find a place to stay"
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+            value={userInput}
+          />
+        </div>
+        {optionList}
+      </React.Fragment>
+    );
+  }
 }
 
 export default SearchBar;
--- src/components/layout/LandingPage.js
+++ src/components/layout/LandingPage.js
@@ -5,69 +5,80 @@ import EstablishmentItems from "./EstablishmentItems";
 import SearchBar from "../functional/SearchBar";
 
 function LandingPage() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
   const [establishments, setEstablishments] = useState([]);
-  const [establishmentFilter, setEstablishmentFilter] = useState([]);
 
   useEffect(() => {
     fetch(GET_ESTABLISHMENTS)
       .then((response) => response.json())
-      .then((json) => {
-        setEstablishments(json);
-        setEstablishmentFilter(json);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+      .then(
+        (json) => {
+          setIsLoaded(true);
+          setEstablishments(json);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, [])
 
-  const filterEstablishments = function (event) {
-    const searchLowercase = event.target.value.toLowerCase();
+  var optionName = [];
 
-    const filterArray = establishments.filter(function (establish) {
-      const lowercaseName = establish.establishmentName.toLowerCase();
-      if (lowercaseName.includes(searchLowercase)) {
-        return true;
-      }
-      return false;
-    });
+  for (let i = 0; i < establishments.length; i++) {
+    optionName.push(establishments[i].establishmentName);
+  }
 
-    setEstablishmentFilter(filterArray);
-  };
 
-  return (
-    <>
-      <Banner />
-      <h1 className="establishment__h1" id="establishment">
-        Places to stay
-      </h1>
-      {establishmentFilter.map((datas) => {
-        const { establishmentName, id } = datas;
-        return (
-          <React.Fragment key={id}>
-            <SearchBar
-              handleSearch={filterEstablishments}
-              value={establishmentName}
-              id={id}
-            />
-          </React.Fragment>
-        );
-      })}[]
-      <SearchBar handleSearch={filterEstablishments} />
-      <div className="establishment">
-        {establishments.map((data) => {
-          const { establishmentName, imageUrl, description, id } = data;
-          return (
-            <React.Fragment key={id}>
-              <EstablishmentItems
-                name={establishmentName}
-                image={imageUrl}
-                description={description}
-                id={id}
-              />
-            </React.Fragment>
-          );
-        })}
-      </div>
-    </>
-  );
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+
+
+    return (
+      
+      <>
+        <Banner />
+        <h1 className="establishment__h1" id="establishment">
+          Places to stay
+        </h1>
+        <SearchBar options={optionName} />
+        <div className="establishment">
+          {establishments.map((data) => {
+            const { establishmentName, imageUrl, description, id } = data;
+            return (
+              <React.Fragment key={id}>
+                <EstablishmentItems
+                  name={establishmentName}
+                  image={imageUrl}
+                  description={description}
+                  id={id}
+                />
+              </React.Fragment>
+            );
+          })}
+        </div>
+      </>
+    );
+
+  }
+
+  //  const filterEstablishments = function (event) {
+  //    const searchLowercase = event.target.value.toLowerCase();
+  //
+  //    const filterArray = establishments.filter(function (establish) {
+  //      const lowercaseName = establish.establishmentName.toLowerCase();
+  //      if (lowercaseName.includes(searchLowercase)) {
+  //        return true;
+  //      }
+  //      return false;
+  //    });
+  //
+  //    setEstablishmentFilter(filterArray);
+  //  };
 }
 
 export default LandingPage;
