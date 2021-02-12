2d6371098b9c1fb41bec4d6c8719e8cc1249cf7f
Sondre Nordjore <sonoskole@outlook.com>
2021-02-12 13:53:52 +0100

    chance to change ports in api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,8 @@
-export const GET_ESTABLISHMENTS = "http://localhost/hotel-api/get-establishments.php";
-export const GET_MESSAGES = "http://localhost/hotel-api/get-contacts.php";
-export const GET_ENQUIRIES = "http://localhost/hotel-api/get-enquiries.php";
-export const POST_ESTABLISHMENTS = "http://localhost/hotel-api/add-establishments-success.php";
-export const POST_MESSAGES = "http://localhost/hotel-api/contact-success.php";
-export const POST_ENQUIRIES = "http://localhost/hotel-api/enquiry-success.php";
+const port = 80;
+
+export const GET_ESTABLISHMENTS = "http://localhost:" + port + "/hotel-api/get-establishments.php";
+export const GET_MESSAGES = "http://localhost:" + port + "/hotel-api/get-contacts.php";
+export const GET_ENQUIRIES = "http://localhost:" + port + "/hotel-api/get-enquiries.php";
+export const POST_ESTABLISHMENTS = "http://localhost:" + port + "/hotel-api/add-establishments-success.php";
+export const POST_MESSAGES = "http://localhost:" + port + "/hotel-api/contact-success.php";
+export const POST_ENQUIRIES = "http://localhost:" + port + "/hotel-api/enquiry-success.php";
